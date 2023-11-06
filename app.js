92ae45b9ee59476d396bb438af8e9451e273fb7a
Nadim Rai <nadim_rai@yahoo.com>
2023-11-06 19:23:48 +0800

    created dino constructor function

---
+++ app.js
@@ -0,0 +1,10 @@
+ // Create Dino Constructor
+ function Dino( species, weight, height, diet, where, when, fact){
+    this.species = species;
+    this.height = height;
+    this.weight = weight;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+}
