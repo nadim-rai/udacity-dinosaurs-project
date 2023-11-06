00130ace8c4e3d0432dc096c9924d69a88f4b24d
Nadim Rai <nadim_rai@yahoo.com>
2023-11-06 19:28:43 +0800

    imported json file and created dinosaur objects

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import dinoJson from './dino.json' assert {type: 'json'}
+
  // Create Dino Constructor
  function Dino( species, weight, height, diet, where, when, fact){
     this.species = species;
@@ -8,3 +10,8 @@
     this.when = when;
     this.fact = fact;
 }
+
+const dinosData = dinoJson.Dinos.map(dino =>{
+    let { species, weight, height, diet, where, when, fact } = dino;
+    return new Dino(species, weight, height, diet, where, when, fact )
+})
